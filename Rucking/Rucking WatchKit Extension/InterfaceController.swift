0ec18b5ff03187d95a079c128d42484b1515fcc7
Dan Smtih <dan.smith@kinandcarta.com>
2021-03-20 08:59:03 +0000

    Rucking intial project setup

---
+++ Rucking/Rucking WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Rucking WatchKit Extension
+//
+//  Created by Dan Smith on 20/03/2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
